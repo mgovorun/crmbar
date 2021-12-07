7371eef08bf25c920e67b1189a3a5f8068cd62d6
Mikhail Govorun <migovorun@gmail.com>
2021-12-07 13:28:08 +0300

    1.0.27

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ if(!serialPort) {
 }
 console.log(serialPort);
 log.info(serialPort);
-const vendors = ['1eab','a108','28e9','oc2e'];
+const vendors = ['1eab','a108','28e9','oc2e','0483'];
 
 
 let mainWindow, tray;
