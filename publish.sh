96afac4d29cc78f2b3ba01cd727b84a3ae42bcc7
Mikhail Govorun <migovorun@gmail.com>
2021-12-07 15:04:15 +0300

    publish.sh

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+vim package.json
+
+ver=`jq -r '.version' package.json`
+
+echo $ver
+
+vipe | gh release create v$ver -F -d -t $ver
+
+GH_TOKEN=`cat private/GH_TOKEN.txt` npm run release
+
+git commit -am $ver
+
+git push
+
+echo "Waiting 6 minutes for appveyor..."
+sleep 360 
+
+gh release list v$ver | grep crmbar-Setup-$ver.exe 
+
+echo "Don't forget to publish release v$ver"
